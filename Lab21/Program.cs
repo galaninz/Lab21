2a42b541a576a2335b2f8903edf9798d5f90a9bb
Zakhar Galanin <zakhargalanin@gmail.com>
2022-07-23 23:11:40 -0700

    Add project files.

---
+++ Lab21/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab21
+{
+    internal class Program
+    {
+        //Интересная задача!
+        const int n = 10;
+        static int[,] path = new int[n, n];
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    path[i, j] = r.Next(0, 99);
+                    Console.Write(" {0:00.} ", path[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            ThreadStart threadStart = new ThreadStart(Gardner1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+
+            Gardner2();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(" {0:0.} ", path[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Gardner1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (path[i, j] >= 0)
+                    {
+                        int delay = path[i, j];
+                        path[i, j] = -1;
+                        Thread.Sleep(delay);
+                    }
+                }
+            }
+        }
+        static void Gardner2()
+        {
+            for (int i = n - 1; i >= 0; i--)
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    if (path[i, j] >= 0)
+                    {
+                        int delay = path[i, j];
+                        path[i, j] = -2;
+                        Thread.Sleep(delay);
+                    }
+                }
+            }
+        }
+    }
+}
